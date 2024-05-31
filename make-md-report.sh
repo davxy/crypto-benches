39edd39f53c4e6f6c41407dea1e80e253b98a9bc
Davide Galassi <davxy@datawok.net>
2024-05-31 11:59:29 +0200

    Update

--- make-md-report.sh
+++ make-md-report.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+search_path="./target/criterion"
 # Check if a path was provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <path>"
-  exit 1
+if [[ -n "$1" ]]; then
+  search_path=$1
 fi
-
-# Get the specified path from the first argument
-search_path=$1
 
 # Define the command to run on each HTML file
 command="your_command_here"
